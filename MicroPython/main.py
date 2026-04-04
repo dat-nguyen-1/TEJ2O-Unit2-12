2616c5fbb3c9c4f08fb730872e3cbd59ecbe0c2f
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-04-04 14:40:44 -0400

    Modify sonar module to control Neopixels based on distance
    Updated the module to turn Neopixels red if an object is within 10 cm.

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,11 @@
 """
 Created by: Dat Nguyen
-Created on: Mar 2026
-This module will calculate distance from the sonar.
+Created on: Apr 2026
+This module will turn all Neopixels red if there is an object wihin 10 cm.
 """
 
 from microbit import *
+from neopixel import NeoPixel
 
 
 class HCSR04:
@@ -63,8 +64,17 @@ class HCSR04:
         return round(((pre + (index_end - index_start) * 8 + post) * 8 * 0.0344) * 0.5)
 
 
-# create sonar instance
+# initialize sonar instance
 sonar = HCSR04(trigger_pin=pin1, echo_pin=pin2, sclk_pin=pin13)
+
+# define color constants
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+
+# initialize neopixel strip
+NEOPIXEL_STRIP = NeoPixel(pin16, 4)
+NEOPIXEL_STRIP.clear()
+NEOPIXEL_STRIP.show()
 
 # initialize display
 display.clear()
@@ -75,9 +85,18 @@ while True:
     if button_a.was_pressed():
         # display distance
         distance = sonar.get_distance_cm()
-        display.clear()
-        display.scroll(str(distance) + " cm")
 
-        # reset display
-        display.clear()
-        display.show(Image.HAPPY)
+        # turn on neopixels according to distance
+        if distance < 10:
+            NEOPIXEL_STRIP[0] = RED
+            NEOPIXEL_STRIP[1] = RED
+            NEOPIXEL_STRIP[2] = RED
+            NEOPIXEL_STRIP[3] = RED
+        else:
+            NEOPIXEL_STRIP[0] = GREEN
+            NEOPIXEL_STRIP[1] = GREEN
+            NEOPIXEL_STRIP[2] = GREEN
+            NEOPIXEL_STRIP[3] = GREEN
+
+        # show Neopixels
+        NEOPIXEL_STRIP.show()
